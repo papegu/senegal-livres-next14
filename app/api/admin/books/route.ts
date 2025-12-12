52bc45480acefe23b340ac167739f9556325a852
papeelhadji <42720756+papegu@users.noreply.github.com>
2025-12-13 00:14:14 +0100

    Add dynamic export and revalidate settings

--- app/api/admin/books/route.ts
+++ app/api/admin/books/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 import { prisma } from "@/lib/prisma";
 import { v4 as uuidv4 } from "uuid";
 import { verifyJwt } from "@/utils/jwt";
