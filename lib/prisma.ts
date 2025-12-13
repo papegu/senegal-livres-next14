7ebbb362068157cba0b09c80dc3cd1c49ce82691
papeelhadji <42720756+papegu@users.noreply.github.com>
2025-12-13 06:31:27 +0100

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,9 +7,10 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["error", "warn"],
+    log: ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
+
