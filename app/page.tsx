b8cd98acf14560f665531b6055e1770472c54fa9
papegu <papeabdoulaye.gueye@uadb.edu.sn>
2025-12-21 01:07:11 +0100

    fix: fermeture correcte des balises JSX sur la page d'accueil

--- app/page.tsx
+++ app/page.tsx
@@ -74,5 +74,8 @@ export default function Home() {
             <b>Livraison partout sur Dakar</b> pour toute commande en ligne.<br />
             <b>Si tu achètes en ligne</b>, tu reçois aussi la version PDF du livre immédiatement après paiement !
           </p>
+        </div>
+      </div>
+    </div>
   );
 }
